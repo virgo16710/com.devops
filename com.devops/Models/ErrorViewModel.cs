3834882ee202a5b0e60c3ee579ea41550cef2160
virgo16710 <pablos15@hotmail.com>
2025-04-08 21:55:39 -0400

    Agregar archivos de proyecto.

---
+++ com.devops/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace com.devops.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
